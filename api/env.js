4f921cc20902e3712bbf6e310613a63d4a83b89d
pepepica12 <almalic8112@gmail.com>
2025-12-06 19:40:50 -0600

    Update env.js
    Ye

--- api/env.js
+++ api/env.js
@@ -1,3 +1,6 @@
 export default function handler(req, res) {
-  res.json({ secret: process.env.VERCEL_PLATFORM_PROTECTION || 'not set' });
+  res.json({
+    secret: process.env.VERCEL_PLATFORM_PROTECTION || 'not set',
+    allEnv: process.env
+  });
 }
