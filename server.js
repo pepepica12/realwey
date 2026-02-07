9fa912baa1f1a5259b36d7f4d44115042b3bf278
Alma <tu_correo@ejemplo.com>
2026-02-06 21:54:46 -0600

    Agrego ruta /rss para leer feed RSS

--- server.js
+++ server.js
@@ -2,3 +2,15 @@
 import app from './index.js';
 
 export default app;
+=======
+mport { leerRSS } from "./rss.js";
+
+const express = require('express');
+const app = express();
+
+app.get("/rss", leerRSS);
+app.get('/api/env', (req, res) => {
+  res.json({ secret: process.env.VERCEL_PLATFORM_PROTECTION || 'not set' });
+});
+
+module.exports = app;2d9ef45 (Agrego ruta /rss para leer feed RSS)
